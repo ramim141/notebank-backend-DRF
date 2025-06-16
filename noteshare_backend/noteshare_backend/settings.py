ca1bac3eee45bd2751c9a2df725f82bd4812efa9
ramim141 <ahramu584@.com>
2025-06-16 23:36:18 +0600

    Setting updated

--- noteshare_backend/noteshare_backend/settings.py
+++ noteshare_backend/noteshare_backend/settings.py
@@ -24,7 +24,13 @@ SECRET_KEY = config('SECRET_KEY')
 
 
 DEBUG = config('DEBUG', default=True, cast=bool)
-ALLOWED_HOSTS = ["*"]
+# settings.py
+ALLOWED_HOSTS = [
+    'edumetro.onrender.com',
+  
+    '127.0.0.1',
+    'localhost',
+]
 
 
 # Application definition
